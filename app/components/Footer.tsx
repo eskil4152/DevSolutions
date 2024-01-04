0f7d9f150c28487773e8951d447c322a6166d391
eskil <eskiltb@hotmail.com>
2024-01-04 14:51:53 +0100

    Dark and light mode added to header and footer. Package json next.js version fix.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,9 @@
 export const Footer = () => {
   return (
-    <div className="bottom-0 absolute border-t-2 w-full py-2 pl-4 border-black">
+    <div
+      id="footer"
+      className="bottom-0 absolute border-t-2 w-full py-2 pl-4 border-black"
+    >
       <ul>
         <li>About Us</li>
         <li>FAQ</li>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,10 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className="flex justify-between items-center border-b-2 border-black p-4 relative">
+    <div
+      id="header"
+      className="flex justify-between items-center border-b-2 border-black p-4 relative"
+    >
       <Link
         className="w-1/3"
         href={{
