f58995c00d7502c5f43704ec4bff09a21ab715c7
eskil <eskiltb@hotmail.com>
2024-01-05 14:13:11 +0100

    Added About Page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <div id="footer" className="bottom-0 border-t-2 py-2 pl-4 border-black">
+      <Link
+        href={{
+          pathname: "/about",
+        }}
+      >
+        <h3 className="font-bold text-lg">About Us</h3>
+      </Link>
       <ul>
-        <li>About Us</li>
         <li>FAQ</li>
         <li>Customer Service</li>
       </ul>
