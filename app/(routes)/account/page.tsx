085360ddb21a59d52510427de850e367eb822dc1
eskil <eskiltb@hotmail.com>
2024-01-06 15:09:07 +0100

    Made non-functioning login page

--- app/(routes)/account/page.tsx
+++ app/(routes)/account/page.tsx
@@ -1,12 +1,33 @@
 export default function Account() {
   return (
-    <div className="text-center">
-      <h1>This is the account page.</h1>
-      <p>Here you should be able to log in, or register an account.</p>
-      <p>
-        If you are logged in, you should be able to view your orders, change
-        your password, log out, and more
-      </p>
+    <div className="text-center ">
+      <h1 className="text-xl font-semibold mt-2">Log In / Register</h1>
+
+      <input
+        type="text"
+        id="username"
+        width={10}
+        height={5}
+        placeholder="Username / Email"
+        className="border-2 border-black my-2"
+      />
+
+      <br />
+
+      <input
+        type="text"
+        id="username"
+        width={10}
+        height={5}
+        placeholder="Password"
+        className="border-2 border-black my-2"
+      />
+
+      <br />
+
+      <button className="border-2 border-black px-4 rounded-full">
+        Log In / Register
+      </button>
     </div>
   );
 }
