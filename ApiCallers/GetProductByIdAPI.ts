4812bfa1d7841e93893376d4666d122458688a8b
eskil <eskiltb@hotmail.com>
2024-01-16 13:08:07 +0100

    fetch update

--- ApiCallers/GetProductByIdAPI.ts
+++ ApiCallers/GetProductByIdAPI.ts
@@ -5,7 +5,7 @@ import useLoading from "../app/tools/UseLoading";
 
 export default function GetProductsById(id: string) {
   const { loading, error, response } = useLoading(
-    async () => await fetchJSON("http://localhost:8080/api/product/" + id)
+    async () => await fetchJSON("http://localhost:8080/api/products/" + id)
   );
 
   return { loading, error, response };
--- ApiCallers/GetProductsAPI.ts
+++ ApiCallers/GetProductsAPI.ts
@@ -5,7 +5,7 @@ import useLoading from "../app/tools/UseLoading";
 
 export default function GetProducts() {
   const { loading, error, response } = useLoading(
-    async () => await fetchJSON("http://localhost:8080/api/product/all")
+    async () => await fetchJSON("http://localhost:8080/api/products/all")
   );
 
   return { loading, error, response };
