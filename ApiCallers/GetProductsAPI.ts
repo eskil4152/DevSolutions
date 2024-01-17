1026b94f97ab35184bfe873bdaa37e8d7da41a4d
eskil <eskiltb@hotmail.com>
2024-01-17 12:47:09 +0100

    Error logging

--- ApiCallers/GetProductsAPI.ts
+++ ApiCallers/GetProductsAPI.ts
@@ -5,6 +5,7 @@ import useLoading from "../app/tools/UseLoading";
 
 export default function GetProducts() {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  console.log("API URL: ", apiUrl);
 
   const { loading, error, response } = useLoading(
     async () => await fetchJSON(`${apiUrl}/api/products/all`)
