2ad72fd2a633bdc3b5cf9fb0acdf78a63c2897d0
eskil <eskiltb@hotmail.com>
2024-01-30 15:45:16 +0100

    Mods can be promoted and demoted

--- app/(routes)/admin/mods/page.tsx
+++ app/(routes)/admin/mods/page.tsx
@@ -1,9 +1,35 @@
 "use client";
 
+import { useState } from "react";
+import ChangeRoleAPI from "@/ApiCallers/Admin/ChangeRoleAPI";
+import { UserRoleChange } from "@/Enums/UserRoleChange";
 import GetAllModeratorsAPI from "@/ApiCallers/Admin/GetAllModeratorsAPI";
 
-export default function AdminModerators() {
+export default function AdminUsers() {
   const { loading, error, response } = GetAllModeratorsAPI();
+
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [userToChange, setUserToChange] = useState<UserType | null>(null);
+  const [action, setAction] = useState<UserRoleChange | null>(null);
+  const [changeError, setChangeError] = useState("");
+
+  function handlePromoteClick(user: UserType) {
+    setUserToChange(user);
+    setShowConfirmation(true);
+  }
+
+  function handleConfirmPromote(id: number, username: string) {
+    try {
+      ChangeRoleAPI(id, username, action!);
+      setShowConfirmation(false);
+    } catch (e) {
+      setChangeError("Failed to change user role - " + e);
+    }
+  }
+
+  function handleCancelPromote() {
+    setShowConfirmation(false);
+  }
 
   while (loading) {
     return (
@@ -19,15 +45,64 @@ export default function AdminModerators() {
         {response.data[0] == null ? (
           <h1>No people</h1>
         ) : (
-          response.data.map((user: any) => (
+          response.data.map((user: UserType) => (
             <div key={user.id} className="py-4">
               <h1>{user.id}</h1>
               <h1>
                 {user.firstname} {user.lastname}
               </h1>
               <h1>{user.email}</h1>
+              <button
+                onClick={() => {
+                  handlePromoteClick(user);
+                  setAction(UserRoleChange.PROMOTE);
+                }}
+              >
+                PROMOTE
+              </button>
+              <button
+                onClick={() => {
+                  handlePromoteClick(user);
+                  setAction(UserRoleChange.DEMOTE);
+                }}
+              >
+                DEMOTE
+              </button>
             </div>
           ))
+        )}
+
+        {showConfirmation && (
+          <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-4">
+              <p>{`Are you sure you want to promote '${userToChange?.username}' to moderator?`}</p>
+              <div className="flex gap-2 justify-center">
+                <button
+                  onClick={() => {
+                    userToChange != null
+                      ? handleConfirmPromote(
+                          userToChange.id,
+                          userToChange.username,
+                        )
+                      : setChangeError("Unable to promote, user not found");
+                  }}
+                  className="font-semibold"
+                >
+                  Confirm
+                </button>
+
+                <p> | </p>
+
+                <button
+                  onClick={handleCancelPromote}
+                  className="font-semibold "
+                >
+                  Cancel
+                </button>
+              </div>
+              <p>{changeError}</p>
+            </div>
+          </div>
         )}
       </div>
     );
@@ -40,13 +115,13 @@ export default function AdminModerators() {
   } else if (error) {
     return (
       <div>
-        <h1>Error occured: {error.message}</h1>
+        <h1>Error occurred: {error.message}</h1>
       </div>
     );
   } else {
     return (
       <div>
-        <h1>Unknown error occured</h1>
+        <h1>Unknown error occurred</h1>
       </div>
     );
   }
